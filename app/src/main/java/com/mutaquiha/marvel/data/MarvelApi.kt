8993ac9b5dac141e5a06a7949bb03e26502eee36
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-06 15:38:47 -0300

    replace hardcoded strings with constants

--- app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
+++ app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
@@ -2,7 +2,12 @@ package com.mutaquiha.marvel.data
 
 import com.google.gson.GsonBuilder
 import com.mutaquiha.marvel.commons.Constants.API_KEY
+import com.mutaquiha.marvel.commons.Constants.BASE_URL
+import com.mutaquiha.marvel.commons.Constants.PARAMETER_API_KEY
+import com.mutaquiha.marvel.commons.Constants.PARAMETER_HASH
+import com.mutaquiha.marvel.commons.Constants.PARAMETER_TIMESTAMP
 import com.mutaquiha.marvel.commons.Constants.PRIVATE_KEY
+import com.mutaquiha.marvel.commons.Constants.TIMEZONE
 import com.mutaquiha.marvel.commons.extensions.md5
 import com.mutaquiha.marvel.data.dto.CharactersResponse
 import okhttp3.OkHttpClient
@@ -14,7 +19,7 @@ import java.util.*
 
 interface MarvelApi {
     @GET("characters")
-    suspend fun getCharacters() : CharactersResponse
+    suspend fun getCharacters(): CharactersResponse
 
     companion object {
         fun getService(): MarvelApi {
@@ -27,11 +32,12 @@ interface MarvelApi {
                 val original = chain.request()
                 val originalHttpUrl = original.url
 
-                val ts = (Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis / 1000L).toString()
+                val timestamp =
+                    (Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE)).timeInMillis / 1000L).toString()
                 val url = originalHttpUrl.newBuilder()
-                    .addQueryParameter("apikey", API_KEY)
-                    .addQueryParameter("ts", ts)
-                    .addQueryParameter("hash", "$ts$PRIVATE_KEY$API_KEY".md5())
+                    .addQueryParameter(PARAMETER_API_KEY, API_KEY)
+                    .addQueryParameter(PARAMETER_TIMESTAMP, timestamp)
+                    .addQueryParameter(PARAMETER_HASH, "$timestamp$PRIVATE_KEY$API_KEY".md5())
                     .build()
 
                 chain.proceed(original.newBuilder().url(url).build())
@@ -39,12 +45,12 @@ interface MarvelApi {
 
             val gson = GsonBuilder().setLenient().create()
             val retrofit = Retrofit.Builder()
-                .baseUrl("http://gateway.marvel.com/v1/public/")
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(httpClient.build())
                 .build()
 
-            return retrofit.create<MarvelApi>(MarvelApi::class.java)
+            return retrofit.create(MarvelApi::class.java)
         }
     }
 }
