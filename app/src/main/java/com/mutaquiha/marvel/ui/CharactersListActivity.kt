bc89e8ed9bfc31024d6fe9c9e108f5cbce2912b6
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-06 14:59:21 -0300

    create CharactersListActivity.kt

--- app/src/main/java/com/mutaquiha/marvel/ui/CharactersListActivity.kt
+++ app/src/main/java/com/mutaquiha/marvel/ui/CharactersListActivity.kt
@@ -1,11 +1,12 @@
-package com.mutaquiha.marvel
+package com.mutaquiha.marvel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mutaquiha.marvel.R
 
-class MainActivity : AppCompatActivity() {
+class CharactersListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_characters_list)
     }
 }
