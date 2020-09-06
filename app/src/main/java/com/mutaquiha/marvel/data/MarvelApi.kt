a4d755dae86c768855971ac6b4c3456397be6f48
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-06 16:37:33 -0300

    remove companion that generated service, it'll be provided by injection

--- app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
+++ app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
@@ -1,56 +1,9 @@
 package com.mutaquiha.marvel.data
 
-import com.google.gson.GsonBuilder
-import com.mutaquiha.marvel.commons.Constants.API_KEY
-import com.mutaquiha.marvel.commons.Constants.BASE_URL
-import com.mutaquiha.marvel.commons.Constants.PARAMETER_API_KEY
-import com.mutaquiha.marvel.commons.Constants.PARAMETER_HASH
-import com.mutaquiha.marvel.commons.Constants.PARAMETER_TIMESTAMP
-import com.mutaquiha.marvel.commons.Constants.PRIVATE_KEY
-import com.mutaquiha.marvel.commons.Constants.TIMEZONE
-import com.mutaquiha.marvel.commons.extensions.md5
 import com.mutaquiha.marvel.data.dto.CharactersResponse
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import java.util.*
 
 interface MarvelApi {
     @GET("characters")
     suspend fun getCharacters(): CharactersResponse
-
-    companion object {
-        fun getService(): MarvelApi {
-            val logging = HttpLoggingInterceptor()
-            logging.level = HttpLoggingInterceptor.Level.BODY
-
-            val httpClient = OkHttpClient.Builder()
-            httpClient.addInterceptor(logging)
-            httpClient.addInterceptor { chain ->
-                val original = chain.request()
-                val originalHttpUrl = original.url
-
-                val timestamp =
-                    (Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE)).timeInMillis / 1000L).toString()
-                val url = originalHttpUrl.newBuilder()
-                    .addQueryParameter(PARAMETER_API_KEY, API_KEY)
-                    .addQueryParameter(PARAMETER_TIMESTAMP, timestamp)
-                    .addQueryParameter(PARAMETER_HASH, "$timestamp$PRIVATE_KEY$API_KEY".md5())
-                    .build()
-
-                chain.proceed(original.newBuilder().url(url).build())
-            }
-
-            val gson = GsonBuilder().setLenient().create()
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(httpClient.build())
-                .build()
-
-            return retrofit.create(MarvelApi::class.java)
-        }
-    }
 }
