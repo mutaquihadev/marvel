8c9663a892fd7da8880d08cb6ae2de80f704e02c
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-06 15:32:15 -0300

    add Constants.kt

---
+++ app/src/main/java/com/mutaquiha/marvel/commons/Constants.kt
@@ -0,0 +1,15 @@
+package com.mutaquiha.marvel.commons
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val TIMEZONE = "UTC"
+
+    const val PARAMETER_API_KEY = "apikey"
+    const val PARAMETER_TIMESTAMP = "ts"
+    const val PARAMETER_HASH = "hash"
+
+    const val API_KEY = "YOUR_API_KEY"
+    const val PRIVATE_KEY = "YOUR_PRIVATE_KEY"
+}
+
