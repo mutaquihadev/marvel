4e13a452a57cc2fc64dc1a20fe1c9acb6657f781
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-06 15:44:46 -0300

    Test get all characters API call

--- app/src/main/java/com/mutaquiha/marvel/ui/CharactersListActivity.kt
+++ app/src/main/java/com/mutaquiha/marvel/ui/CharactersListActivity.kt
@@ -2,11 +2,25 @@ package com.mutaquiha.marvel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.mutaquiha.marvel.R
+import com.mutaquiha.marvel.data.MarvelApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import java.lang.StringBuilder
 
 class CharactersListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_characters_list)
+
+        CoroutineScope(Main).launch {
+            val stringBuilder = StringBuilder()
+            MarvelApi.getService().getCharacters().data.results.forEach {
+                    stringBuilder.append("${it.name}\n")
+            }
+            Toast.makeText(this@CharactersListActivity, stringBuilder.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
