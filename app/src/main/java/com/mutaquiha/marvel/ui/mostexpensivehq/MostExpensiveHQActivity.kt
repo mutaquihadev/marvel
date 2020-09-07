5a9881a65c55586477bf39c650cce07178b5c8a0
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-07 17:40:14 -0300

    getComics on MostExpensiveHQViewModel.kt

--- app/src/main/java/com/mutaquiha/marvel/ui/mostexpensivehq/MostExpensiveHQActivity.kt
+++ app/src/main/java/com/mutaquiha/marvel/ui/mostexpensivehq/MostExpensiveHQActivity.kt
@@ -1,6 +1,7 @@
 package com.mutaquiha.marvel.ui.mostexpensivehq
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.mutaquiha.marvel.R
@@ -14,5 +15,11 @@ class MostExpensiveHQActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_most_expensive_h_q)
+
+        viewModel.comics.observe(this, {
+            Toast.makeText(this, "${it.code} - ${it.comicData.count}", Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.getComics()
     }
 }
--- app/src/main/java/com/mutaquiha/marvel/ui/mostexpensivehq/MostExpensiveHQViewModel.kt
+++ app/src/main/java/com/mutaquiha/marvel/ui/mostexpensivehq/MostExpensiveHQViewModel.kt
@@ -2,19 +2,30 @@ package com.mutaquiha.marvel.ui.mostexpensivehq
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.mutaquiha.marvel.commons.Constants
-import com.mutaquiha.marvel.data.repositories.CharacterRepository
+import com.mutaquiha.marvel.data.dto.comics.ComicResponse
+import com.mutaquiha.marvel.data.repositories.ComicsRepository
 import com.mutaquiha.marvel.domain.entity.Character
+import kotlinx.coroutines.launch
 
 class MostExpensiveHQViewModel @ViewModelInject constructor(
-    private val repository: CharacterRepository,
+    private val repository: ComicsRepository,
     @Assisted private val savedStateHandle: SavedStateHandle
 ) : ViewModel(), LifecycleObserver {
 
     private val character: Character? by lazy {
         savedStateHandle.get<Character>(Constants.KEY_CHARACTER)
+    }
+
+    val comics: MutableLiveData<ComicResponse> = MutableLiveData()
+
+    fun getComics() {
+        character?.let {
+            viewModelScope.launch {
+                val response = repository.getComics(characterId = it.id)
+                comics.postValue(response)
+            }
+        }
     }
 }
