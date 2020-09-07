c67bc27e8add092856b56a49a5dedd06877060b2
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-07 17:35:37 -0300

    Add getComics HttpCall in MarvelApi

--- app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
+++ app/src/main/java/com/mutaquiha/marvel/data/MarvelApi.kt
@@ -1,10 +1,15 @@
 package com.mutaquiha.marvel.data
 
-import com.mutaquiha.marvel.data.dto.CharactersResponse
+import com.mutaquiha.marvel.data.dto.characters.CharactersResponse
+import com.mutaquiha.marvel.data.dto.comics.ComicResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MarvelApi {
     @GET("characters")
     suspend fun getCharacters(@Query("offset") offset: Int? = 0): CharactersResponse
+
+    @GET("characters/{characterId}/comics")
+    suspend fun getComics(@Path("characterId") characterId: Int) : ComicResponse
 }
