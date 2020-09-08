c8d2cf02ea32795018e4a0c5dc9d3442126f026e
Olivio Mutaquiha <olivio.mutaquiha@gmail.com>
2020-09-08 14:00:31 -0300

    Create DomainEntityExtensions.kt file

--- app/src/main/java/com/mutaquiha/marvel/commons/extensions/DomainEntityExtensions.kt
+++ app/src/main/java/com/mutaquiha/marvel/commons/extensions/DomainEntityExtensions.kt
@@ -2,8 +2,13 @@ package com.mutaquiha.marvel.commons.extensions
 
 import com.mutaquiha.marvel.commons.ImageSize
 import com.mutaquiha.marvel.domain.entity.Character
+import com.mutaquiha.marvel.domain.entity.Comic
 
 
 fun Character.getImageUrl(sizeOfImage: String = ImageSize.STANDARD_MEDIUM): String {
     return "${this.Thumbnail.path}/$sizeOfImage.${this.Thumbnail.extension}"
+}
+
+fun Comic.getImageUrl(sizeOfImage: String): String {
+    return "${this.thumbnail.path}/$sizeOfImage.${this.thumbnail.extension}"
 }
